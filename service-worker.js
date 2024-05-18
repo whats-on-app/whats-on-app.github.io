6ba55e662993c122c83975bfa93bd3b051fb4b45
whats-on-app <toucheshoutbox@gmail.com>
2024-05-18 15:38:57 +0200

    chore: update packages and prettier

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fae1298eda31342e2aca8be903f72c8c.js"
+  "/precache-manifest.d84e4a403627101147e9db0242502ef2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
